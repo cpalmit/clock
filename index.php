24d826b889ba85455c22db41597ecb7984356c00
CP <cpalmit@users.noreply.github.com>
2014-09-25 00:02:38 -0400

    fixed more <?php errors

--- index.php
+++ index.php
@@ -4,16 +4,16 @@
 	<meta charset="utf-8"> 
 	<title>What time is it?</title>
 	<?php require "logic.php"; ?>
-	<link rel='stylesheet' href='styles.css' type='text/css'>		
+	<link rel="stylesheet" href="styles.css" type="text/css">		
 </head>
 
-<body class="<? echo $timeofday ?>">
+<body class="<?php echo $timeofday ?>">
 
-	<h1>It is <? echo $time; ?></h1> 
+	<h1>It is <?php echo $time; ?></h1> 
 	<!--<p>But in the 24 hour clock, it's <? echo $hour .":". $minute; ?></p>-->
 
-	<p>Time zone: <? echo $timezone; ?> </p>
-	<img src='http://thewc.co.s3.amazonaws.com/challenges/php-<?echo $timeofday; ?>.png' alt='Scenery matching the time of day'> 
+	<p>Time zone: <?php echo $timezone; ?> </p>
+	<img src="http://thewc.co.s3.amazonaws.com/challenges/php-<?php echo $timeofday; ?>.png" alt="Scenery matching the time of day"> 
 
 </body>
 </html>
