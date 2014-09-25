ac2ec87a2abea8dd40e63ef7893687ed7d146454
CP <cpalmit@users.noreply.github.com>
2014-09-24 23:47:54 -0400

    first commit of clock exercise

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"> 
+	<title>What time is it?</title>
+	<?php require "logic.php"; ?>
+	<link rel='stylesheet' href='styles.css' type='text/css'>		
+</head>
+
+<body class="<? echo $timeofday ?>">
+
+	<h1>It is <? echo $time; ?></h1> 
+	<!--<p>But in the 24 hour clock, it's <? echo $hour .":". $minute; ?></p>-->
+
+	<p>Time zone: <? echo $timezone; ?> </p>
+	<img src='http://thewc.co.s3.amazonaws.com/challenges/php-<?echo $timeofday; ?>.png' alt='Scenery matching the time of day'> 
+
+</body>
+</html>
---
+++ logic.php
@@ -0,0 +1,16 @@
+<? 
+date_default_timezone_set("America/New_York");
+$timezone = date("e");
+$time = date("h:i a");
+$hour = date("G");
+
+if (($hour >= 5)&&($hour<11)){
+	$timeofday="morning";
+} else if (($hour >= 11)&&($hour<16)){
+	$timeofday="afternoon";
+} else if (($hour >= 16)&&($hour<20)){
+	$timeofday="evening";
+} else if(($hour >= 20)||($hour<5)){
+	$timeofday="night";
+}
+?>
